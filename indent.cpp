2f2dc08468e2846cffe04db05dcece6f05904674
jubuyer <jahmed10@stuy.edu>
2021-10-21 19:46:11 -0400

    Indent can now be called from main with string parameter of file name

--- indent.cpp
+++ indent.cpp
@@ -1,57 +1,80 @@
 #include <iostream>
+#include <fstream>
 #include <cctype>
 #include <string>
 #include "indent.h"
 #include "unindent.h"
 
+
+/*
+TESTING
+
 int main() {
+  std::cout << indent("bad-code.cpp") << '\n';
+	std::cout << indent("bad-code5.cpp") << '\n';
+
+
+  return 0;
+}
+*/
+
+std::string indent(std::string in) {
+	std::fstream file;
 	std::string input;
+	std::string output = "";
 
 	int tabs = 0;
+	int temp_tabs = tabs;
+	file.open(in);
 
-	while (getline(std::cin, input)) {
-		input = removeLeadingSpaces(input);
+	if (file.is_open()) {
+		while (getline(file, input)) {
+			input = removeLeadingSpaces(input);
 
-		if ((input[0] == '}')) {
-			tabs--;
-		} else if (instanceChar(input, '{') && instanceChar(input, '}')) {
-			tabs = tabs;
-		} else if (instanceChar(input, '}')) {
-			tabs--;
-		}
-
-		for (int i = 0; i < tabs; i++) {
-			std::cout << '\t';
-		}
-
-		std::cout << input << '\n';
-
-		if (countChar(input, '}') > 1) {
-			for (int i = 0; i < countChar(input, '}'); i++) {
+			if (input[0] == '}') {
+				tabs--;
+			} else if (instanceChar(input, '{') && instanceChar(input, '}')) {
+				temp_tabs = tabs;
+			} else if (instanceChar(input, '}')) {
 				tabs--;
 			}
 
-			if (instanceChar(input, '{')) {
+			for (int i = 0; i < tabs; i++) {
+				output += '\t';
+			}
+
+			output += input;
+			output += '\n';
+
+			if (countChar(input, '}') > 1) {
+				for (int i = 0; i < countChar(input, '}'); i++) {
+					tabs--;
+				}
+
+				if (instanceChar(input, '{')) {
+					tabs++;
+				}
+			}
+
+			if (countChar(input, '{') > 1) {
+				for (int i = 0; i < countChar(input, '{'); i++) {
+					tabs++;
+				}
+
+				if (instanceChar(input, '}')) {
+					tabs--;
+				}
+			} else if (instanceChar(input, '{') && !instanceChar(input, '}')) {
+				tabs++;
+			} else if ((input[0] == '}') && instanceChar(input, '{')) {
 				tabs++;
 			}
 		}
-
-		if (countChar(input, '{') > 1) {
-			for (int i = 0; i < countChar(input, '{'); i++) {
-				tabs++;
-			}
-
-			if (instanceChar(input, '}')) {
-				tabs--;
-			}
-		} else if (instanceChar(input, '{') && !instanceChar(input, '}')) {
-			tabs++;
-		} else if ((input[0] == '}') && instanceChar(input, '{')) {
-			tabs++;
-		}
-
-	} 
-	return 0;
+		file.close();
+	} else {
+		std::cerr << "Unable to open file.\n";
+	}
+	return output;
 }
 
 bool instanceChar(std::string line, char c) {
--- indent.h
+++ indent.h
@@ -3,3 +3,5 @@
 bool instanceChar(std::string line, char c);
 
 int countChar(std::string line, char c);
+
+std::string indent(std::string in);
