527705b27cb599e996adfdc3d97fedb51b26bc82
jubayer <jubayer7a@gmail.com>
2021-10-21 12:50:02 -0400

    Unindent can be called from main with string parameter of file name

--- unindent.cpp
+++ unindent.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <cctype>
 #include "unindent.h"
 
-std::string unindent() {
-  std::string input;
+/*
+TESTING 
 
-  while(getline(std::cin, input)) {
-    std::cout << removeLeadingSpaces(input) << '\n';
-  }
+int main() {
+  std::cout << unindent("bad-code.cpp") << '\n';
+  std::cout << unindent("bad-code3.cpp") << '\n';
+
 
   return 0;
+}
+*/
+
+std::string unindent(std::string in) {
+  std::fstream file;
+  std::string input;
+  std::string output = "";
+
+  file.open(in);
+  if (file.is_open()) {
+    while(getline(file, input)) {
+      output += removeLeadingSpaces(input); 
+      output += '\n';
+    }
+
+    file.close();
+  } else {
+    std::cerr << "Unable to open file\n";
+  }
+
+
+  return output;
 }
 
 std::string removeLeadingSpaces(std::string line) {
